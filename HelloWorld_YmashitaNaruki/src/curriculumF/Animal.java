833aa6b45523af158985edf8f135b0ec09381904
cheng <cheng@decem>
2026-06-02 00:56:34 +0900

    課題を提出いたします。ご確認お願いいたします。

--- HelloWorld_YmashitaNaruki/src/curriculumF/Animal.java
+++ HelloWorld_YmashitaNaruki/src/curriculumF/Animal.java
@@ -1,5 +1,36 @@
 package curriculumF;
 
 public class Animal {
+	private String name;
+	private double length;
+	private int speed;
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public double getLength() {
+		return this.length;
+	}
+
+	public int getSpeed() {
+		return this.speed;
+	}
+
+	public static String[] split(String string) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 }
